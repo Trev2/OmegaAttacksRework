f22dfc8aa62c901b243f4085019a632a6e8783f2
TrevTheModder <trevermckenzie3@gmail.com>
2024-06-19 12:58:09 -0400

    Add files via upload

--- src/config.lua
+++ src/config.lua
@@ -1,5 +1,15 @@
-return {
-  version = 0;
-  enabled = true;
-  message = 'Hello World!';
+local config = {
+  enabled = true
+  InstantOmegaCast = {
+    Enabled = true
+  }
 }
+
+local description = {
+  enabled = "Set to true to enable the mod. Set to false to disable the mod."
+  InstantOmegaCast = {
+    Enabled = "Enable to make the Omega cast fire instantly when you press the cast button. Will fire basic cast if mana is insufficient."
+  }
+}
+
+return config, description

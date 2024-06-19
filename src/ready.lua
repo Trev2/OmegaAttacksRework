ed250ecbecad1dd4504a701dfc9902b1108590b5
TrevTheModder <trevermckenzie3@gmail.com>
2024-06-19 12:54:00 -0400

    Add files via upload

--- src/ready.lua
+++ src/ready.lua
@@ -7,17 +7,30 @@
 -- 	so you will most likely want to have it reference
 --	values and functions later defined in `reload.lua`.
 
-local file = rom.path.combine(rom.paths.Content, 'Game/Text/en/ShellText.en.sjson')
+if config.InstantOmegaCast.Enabled then
+    local weaponFile = rom.path.combine(rom.paths.Content, 'Game/Weapons/PlayerWeapons.sjson')
 
-sjson.hook(file, function(data)
-	return sjson_ShellText(data)
-end)
+	sjson.hook(weaponFile, function(sjsonData)
+		return sjson_CastWeapon(sjsonData)
+	end)
 
-modutil.mod.Path.Wrap("SetupMap", function(base, ...)
-	prefix_SetupMap()
-	return base(...)
-end)
-
-game.OnControlPressed({'Gift', function()
-	return trigger_Gift()
-end})
+	function sjson_CastWeapon(sjsonData)
+		for _, v in ipairs(sjsonData.Weapons) do
+			if v.Name == "WeaponCast" then
+				v.ForceReleaseOnFire = false
+                v.FullyAutomatic = true
+                v.FireGraphic = "Melinoe_Cast_Fire"
+                v.SwapOnFire = "WeaponCastArm"
+                v.AddOnFire = "null"
+            elseif v.Name = "WeaponCastArm"
+                v.ForceMaxChargeRelease = false
+                v.CancelChargeOnRelease = false
+                v.FireOnRelease = false
+                v.AllowExternalForceRelease = true
+                v.LoseControlOnRelease = false
+                v.ChargeTime = 0.0
+                v.MinChargeToFire = 0.0
+			end
+		end
+	end
+end
